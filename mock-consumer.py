394c029e72f80a62abeb42108c5fbc55379a77be
caian23 <caian23@outlook.com>
2025-11-18 12:27:15 -0300

    feat: new functional mocks of the ndn stack using ndnappv2

--- mock-consumer.py
+++ mock-consumer.py
@@ -15,9 +15,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # -----------------------------------------------------------------------------
-import typing
 import logging
-from ndn import appv2
+from ndn import utils, appv2, types
 from ndn import encoding as enc
 
 
@@ -29,20 +28,31 @@ logging.basicConfig(format='[{asctime}]{levelname}:{message}',
 
 app = appv2.NDNApp()
 
-keychain = app.default_keychain()
 
-@app.route('/app')
-def on_interest(name: enc.FormalName, _app_param: typing.Optional[enc.BinaryStr],
-                reply: appv2.ReplyFunc, context: appv2.PktContext):
-    print(f'>> I: {enc.Name.to_str(name)}, {context["int_param"]}')
-    content = "Hello, world!".encode()
-    reply(app.make_data(name, content=content, signer=keychain.get_signer({}),
-                        freshness_period=10000))
-    print(f'<< D: {enc.Name.to_str(name)}')
-    print(enc.MetaInfo(freshness_period=10000))
-    print(f'Content: (size: {len(content)})')
-    print('')
+async def main():
+    try:
+        timestamp = utils.timestamp()
+        name = enc.Name.from_str('/app') + [enc.Component.from_timestamp(timestamp)]
+        print(f'Sending Interest {enc.Name.to_str(name)}, {enc.InterestParam(must_be_fresh=True, lifetime=6000)}')
+        # TODO: Write a better validator
+        data_name, content, pkt_context = await app.express(
+            name, validator=appv2.pass_all,
+            must_be_fresh=True, can_be_prefix=False, lifetime=6000)
+
+        print(f'Received Data Name: {enc.Name.to_str(data_name)}')
+        print(pkt_context['meta_info'])
+        print(bytes(content) if content else None)
+    except types.InterestNack as e:
+        print(f'Nacked with reason={e.reason}')
+    except types.InterestTimeout:
+        print(f'Timeout')
+    except types.InterestCanceled:
+        print(f'Canceled')
+    except types.ValidationFailure:
+        print(f'Data failed to validate')
+    finally:
+        app.shutdown()
 
 
 if __name__ == '__main__':
-    app.run_forever()
+    app.run_forever(after_start=main())
